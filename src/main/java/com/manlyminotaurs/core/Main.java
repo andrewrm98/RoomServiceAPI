0f2dc99c4768c5c682cef0c65fc1d9c6e8bb8b74
Andrew <armorrison@wpi.edu>
2018-04-17 17:38:46 -0400

    yay

--- src/main/java/com/manlyminotaurs/core/Main.java
+++ src/main/java/com/manlyminotaurs/core/Main.java
@@ -29,7 +29,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         try{
         //root is anchor pane that all other screens will be held in
-        root = FXMLLoader.load(getClass().getClassLoader().getResource("FXMLs/home.fxml"));
+        root = FXMLLoader.load(getClass().getClassLoader().getResource("FXMLs/roomServiceAPI.fxml"));
 
         Scene world = new Scene(root, 1920, 1080);
         primaryStage.setTitle("Brigham and Women's Hospital Navigation");
